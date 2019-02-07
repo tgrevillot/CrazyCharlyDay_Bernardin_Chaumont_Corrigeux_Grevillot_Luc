0b08d063d0ca5a41bd3a41ceded2a587b08312a8
Terry <terry.grevillot5@etu.univ-lorraine.fr>
2019-02-07 13:39:51 +0100

    Implémentation Backend Connexion en tant qu'invité + ajout route dans index pour ceci

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once 'vendor/autoload.php';
 
 use \justjob\bd\ConnectionDB as ConnectionDB;
 use \justjob\vues\VueConnexion as VueConnexion;
+use \justjob\controleurs\ControleAuthentification as ControleAuthentification;
 //use \justjob\controleurs\ContCandidature as ContCandidature;
 
 session_start();
@@ -14,13 +15,23 @@ ConnectionDB::start('src/conf/conf.ini');
 $app = new \Slim\Slim();
 
 $app->get("/", function() {
-    $v = new VueConnexion("connexion");
-    echo $v->render("");
+    $v = new VueConnexion("connexion", null);
+    $v->render("");
 })->name('formConnexion');
 
 $app->get('/candidatures/$id',function($id){
     $c = new c\ContCandidature();
     $c->afficherTout($id);
 });
+
+$app->get("/chooseAccount", function() {
+    ControleAuthentification::chooseUserAccount();
+})->name("inviteChooseAccount");
+
+$app->get("/connectAs/:uid", function($uid) {
+    ControleAuthentification::loadProfile($uid);
+    //TODO : REDIRIGER VERS LA PAGE ACCUEIL :
+    //$app->redirectTo("nomDeLaRoute");
+})->name("connectAs");
 
 $app->run();
